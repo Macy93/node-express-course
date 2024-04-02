165981340b32afb8901a0fbb6cf0077deab4a2ff
Michelle <woodennickel@yahoo.com>
2024-04-02 10:57:38 -0400

    lesson week6

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -2,9 +2,19 @@ const mongoose = require("mongoose");
 
 //setting up the structure for the documents in our collections
 //so string values for list of items in task manager
+//only the properties in the TaskSchema wiil appear in postman and the database,
+// cannot add items directly in Postman otherwise they will be ignored
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must Provide Name"],
+    trim: true, //this will delete extra spaces around the name
+    maxlength: [20, "Name Can Not Be More Than 20 Characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
--- 03-task-manager/starter/wk5app.js
+++ 03-task-manager/starter/wk5app.js
@@ -4,19 +4,30 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect"); //only if we are successful connecting
 //to the db will be try to connect to the server
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 
 //middleware
+app.use(express.static("./public")); // static files location-in public folder
 app.use(express.json()); //this is for the data in req.body
 
 //routes //get request// path or url, callback function
-app.get("/hello", (req, res) => {
-  res.send("Task Manager App");
-});
+//app.get("/hello", (req, res) => {
+//  res.send("Task Manager App");
+//});
 
 //this gets us to the tasks.js page and runs it I guess
 app.use("/api/v1/tasks", tasks);
 
-const port = 3000;
+app.use(notFound);
+app.use(errorHandlerMiddleware);
+
+//instead of hard coding the port, this will allow the system to use
+//whatever port is available
+//this is good for deployment, locally 3000 is good
+//to test this line of code, stop server then $ PORT=6000 node wk5app.js
+//should see server is listening on port 6000
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
